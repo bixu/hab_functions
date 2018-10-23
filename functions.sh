d869bd58562e7858b703fb3f15bb78a17d07f567
Graham Weldon <graham@grahamweldon.com>
2018-10-23 16:39:00 +0900

    Refactor a bit and make it usable for local studio as well as SSH
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- functions.sh
+++ functions.sh
@@ -1,58 +1,74 @@
-#!/bin/bash
+#!/bin/sh
 
+CENSUS_ENDPOINT="http://localhost:9631/census"
+
+# Helper function to display usage information
+__hf_usage() {
+  echo ""
+  echo "Usage: ${1}"
+  echo ""
+}
+
+hab_functions_setup() {
+  hab pkg install --binlink core/curl
+  hab pkg install --binlink core/jq-static
+}
+
+# Display service group leaders
 service_group_leaders() {
-  if [ -z $2 ]
-  then
-    echo ""
-    echo "usage: ${FUNCNAME[0]} '<ssh user@host>' '<service.group>'"
-    echo ""
+  if [ -z ${1} ]; then
+    __hf_usage "${FUNCNAME[0]} <'service.group'> ['ssh user@host']"
     return 1
   fi
+  service_group="${1}"
 
-  ssh_command=$1
-  service_group=$2
+  census_data_command="curl --silent ${CENSUS_ENDPOINT}"
+  if [ ! -z ${2} ]; then
+    census_data_command="${2} ${census_data_command}"
+  fi
 
-  census_data=$($ssh_command 'curl --silent localhost:9631/census')
-  leader_data=$(echo $census_data | jq -r ".census_groups | .\"$service_group\" | .population | .[] | select(.leader == true)")
+  census_data="$(${census_data_command})"
+  leader_data="$(echo "${census_data}" | \
+    jq -r ".census_groups | select(.\"${service_group}\" != null) | .[] | .population | .[] | select(.leader == true)")"
 
-  echo "$(echo $leader_data | jq -r '
-                              "\nHostname: " + "\(.sys | .hostname)" +
-                              "\nAddress: "  + "\(.sys | .ip)" +
-                              "\nAlive: "    + "\(.alive)" +
-                              "\nLeader: "   + "\(.leader)" +
-                              "\nDeparted: " + "\(.departed)" +
-                              "\nMemberID: " + "\(.member_id)" + "\n======"')" | column -t
+  echo "$(echo $leader_data | \
+    jq -r '
+    "\nHostname: " + "\(.sys | .hostname)" +
+    "\nAddress: "  + "\(.sys | .ip)" +
+    "\nAlive: "    + "\(.alive)" +
+    "\nLeader: "   + "\(.leader)" +
+    "\nDeparted: " + "\(.departed)" +
+    "\nMemberID: " + "\(.member_id)" + "\n======"')" | column -t
 }
 
 service_group_members() {
-  if [ -z $2 ]
-  then
-    echo ""
-    echo "usage: ${FUNCNAME[0]} '<ssh user@host>' '<service.group>'"
-    echo ""
+  if [ -z ${1} ]; then
+    __hf_usage "${FUNCNAME[0]} <'service.group'> ['ssh user@host']"
     return 1
   fi
+  service_group="${1}"
 
-  ssh_command=$1
-  service_group=$2
+  census_data_command="curl --silent ${CENSUS_ENDPOINT}"
+  if [ ! -z ${2} ]; then
+    census_data_command="${2} ${census_data_command}"
+  fi
 
-  census_data=$($ssh_command 'curl --silent localhost:9631/census')
-  leader_data=$(echo $census_data | jq -r ".census_groups | .\"$service_group\" | .population | .[] | select(.alive == true)")
+  census_data="$(${census_data_command})"
+  member_data="$(echo "${census_data}" | \
+    jq -r ".census_groups | select(.\"${service_group}\" != null) | .[] | .population | .[] | select(.alive == true)")"
 
-  echo "$(echo $leader_data | jq -r '
-                              "\nHostname: " + "\(.sys | .hostname)" +
-                              "\nLeader: "   + "\(.leader)" +
-                              "\nDeparted: " + "\(.departed)" +
-                              "\nMemberID: " + "\(.member_id)" + "\n======"')" | column -t
+  echo "$(echo $member_data | \
+    jq -r '
+    "\nHostname: " + "\(.sys | .hostname)" +
+    "\nLeader:   " + "\(.leader)" +
+    "\nDeparted: " + "\(.departed)" +
+    "\nMemberID: " + "\(.member_id)" + "\n======"')" | column -t
 }
 
 depart_member() {
-  if [ -z $2 ]
-  then
-    echo ""
-    echo "usage: ${FUNCNAME[0]} '<ssh user@host>' '<member id>'"
-    echo ""
+  if [ -z $2 ]; then
+    __hf_usage "${FUNCNAME[0]} <'member id'> <'ssh user@host'>"
     return 1
   fi
-  $1 "sudo hab sup depart $2"
+  ${1} "sudo hab sup depart ${2}"
 }
